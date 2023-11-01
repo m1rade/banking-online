559c6467dc58c2efbb69e83cb58fb8114f22ca56
m1rade <forx98@gmail.com>
2023-11-01 04:30:16 +0300

    loan money feature

--- script.js
+++ script.js
@@ -161,6 +161,22 @@ btnTransfer.addEventListener('click', function (e) {
     }
 });
 
+btnLoan.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const amount = Number(inputLoanAmount.value);
+
+    // the loan is only granted if there is any deposit
+    // that's greater or equal 10 % of the requested amount of loan
+    if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+        currentAccount.movements.push(amount);
+
+        updateUI(currentAccount);
+    }
+
+    inputLoanAmount.value = '';
+});
+
 btnClose.addEventListener('click', function (e) {
     e.preventDefault();
 
